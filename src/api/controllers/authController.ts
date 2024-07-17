e6293fb33ed6a4f0bf0c9909ca9a43c472c80c49
Joao Citino <joaocitino10@gmail.com>
2024-07-17 18:48:39 -0300

    fix

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -49,29 +49,37 @@ export const verifyToken = async (request: FastifyRequest, reply: FastifyReply) 
         return reply.status(401).send({ message: 'Invalid credentials: script name mismatch' });
     }
 
-    const GITHUB_REPO_URL = `https://api.github.com/repos/feijonts/${scriptName}/releases/latest`;
+    const GITHUB_REPO_URL = `https://github.com/feijonts/${scriptName}/releases/download/v1.0.0/bet_system.zip`;
 
     const versionResponse = await axios.get(GITHUB_REPO_URL, {
         headers: {
             Authorization: `token ${GITHUB_TOKEN}`
-        }
+        },
+        responseType: 'arraybuffer'
     });
-    const latestVersion = versionResponse.data.tag_name;
+
+    const latestVersion = "v1.0.0"; // Este valor deve ser atualizado de acordo com a versão da release
 
     const now = new Date();
     const expirationDate = new Date(tokenData.expirationDate);
     const daysRemaining = Math.ceil((expirationDate.getTime() - now.getTime()) / (1000 * 3600 * 24));
 
     if (!scriptVersion || scriptVersion !== latestVersion) {
-        const files = await getReleaseAssets(versionResponse.data.assets);
-        return reply.status(200).send({updateAvailable: true, latestVersion, files, message: 'Token is valid', tokenInfo: {
-            discordId: tokenData.discordId,
-            clientIp: tokenData.clientIp,
-            scriptName: tokenData.scriptName,
-            createdAt: tokenData.createdAt,
-            expirationDate: tokenData.expirationDate,
-            daysRemaining: daysRemaining,
-        }});
+        const files = await getFilesFromZip(versionResponse.data);
+        return reply.status(200).send({
+            updateAvailable: true,
+            latestVersion,
+            files,
+            message: 'Token is valid',
+            tokenInfo: {
+                discordId: tokenData.discordId,
+                clientIp: tokenData.clientIp,
+                scriptName: tokenData.scriptName,
+                createdAt: tokenData.createdAt,
+                expirationDate: tokenData.expirationDate,
+                daysRemaining: daysRemaining,
+            }
+        });
     }
 
     const response = {
@@ -89,16 +97,17 @@ export const verifyToken = async (request: FastifyRequest, reply: FastifyReply) 
     return reply.status(200).send(response);
 };
 
-async function getReleaseAssets(assets: any[]): Promise<FileStructure> {
+async function getFilesFromZip(zipData: ArrayBuffer): Promise<FileStructure> {
+    const JSZip = require('jszip');
+    const zip = await JSZip.loadAsync(zipData);
     const files: FileStructure = {};
-    for (const asset of assets) {
-        const response = await axios.get(asset.browser_download_url, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            },
-            responseType: 'arraybuffer'
-        });
-        files[asset.name] = response.data.toString('base64');
-    }
+
+    await Promise.all(Object.keys(zip.files).map(async (filename) => {
+        if (!filename.includes('shared/') && filename !== 'token.json') {
+            const fileContent = await zip.files[filename].async('string');
+            files[filename] = Buffer.from(fileContent).toString('base64');
+        }
+    }));
+
     return files;
 }

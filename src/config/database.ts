6df0702c5a8c9ea041c451a496c8c30793f08dac
Joao Citino <joaocitino10@gmail.com>
2024-07-17 02:24:02 -0300

    atts

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,9 +7,13 @@ if (process.env.NODE_ENV !== 'production') {
 
 const pool: Pool = createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
